a7f5590004b1a865a861a4275160bfafdaed282b
Mc <psyence_mc@hotmail.com>
2023-03-08 12:52:12 +0100

    start of n3

---
+++ S04T02N03XavierRue/src/main/java/cat/itacademy/barcelonactiva/xavier/rue/s04/t02/n03/S04T02N03XavierRueApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.xavier.rue.s04.t02.n03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N03XavierRueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N03XavierRueApplication.class, args);
+	}
+
+}
